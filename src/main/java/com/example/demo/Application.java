fac01b67b3e126ce46f75d62b63a95bebc9ad0b6
Manpreet Singh <97432223+Manpreet3033@users.noreply.github.com>
2025-01-16 20:46:31 +0530

    Setup Basic JPA Crud Opertaions

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.models.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student manpreet = new Student("Manpreet","Singh","manpreetkamboj60.net@gmail.com",22);
+            Student testUser1 = new Student("Test","User1","testuser1@gmail.com",18);
+            Student testUser2 = new Student("Test","User2","testuser2@gmail.com",20);
+            studentRepository.save(manpreet);
+            studentRepository.save(testUser1);
+            studentRepository.save(testUser2);
+        };
     }
 
 }
---
+++ src/main/java/com/example/demo/models/Student.java
@@ -0,0 +1,90 @@
+package com.example.demo.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_unique_email", columnNames = "email")
+        }
+)
+@Entity(name = "Student")
+public class Student {
+    public Student(String firstName, String lastName, String email, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    @Id
+    @GenericGenerator(
+            name = "uuid",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @GeneratedValue(
+            generator = "uuid"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private UUID id;
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String firstName;
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String lastName;
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String email;
+    @Column(
+            name = "age",
+            nullable = false
+    )
+    private Integer age;
+
+    public UUID getId(){
+        return id;
+    }
+    public void setId(UUID id){
+        this.id = id;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
